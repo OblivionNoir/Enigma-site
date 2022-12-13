c62fb12638d64a665bd2b386c9d3e04ef2a3552e
thedarklord176 <donahueb85@gmail.com>
2022-12-13 16:26:54 -0600

    misc

--- enigma-site/src/App.jsx
+++ enigma-site/src/App.jsx
@@ -10,25 +10,43 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //logo also acts as home button
 
 
-
+var tags = ["Dark Web",
+    "Internet Culture",
+    "Cybersecurity",
+    "Media/Entertainment",
+    "Scary", "Conspiracies",
+    "Fictional"]
 function AccMenu() {
     //make hover controls
     const [isShown, setIsShown] = useState(false)
     return (
         <React.Fragment>
-            <button onMouseEnter={() => setIsShown(true)}
-            >
+            <button onMouseEnter={() => setIsShown(true)}>
                 Browse by category...
 
                 <div className={(isShown ? 'open' : 'closed')}>
                     <ul onMouseLeave={() => setIsShown(false)}>
-                        <li>Dark Web</li>
-                        <li>Internet Culture</li>
-                        <li>Cybersecurity</li>
-                        <li>Media/Entertainment</li>
-                        <li>Scary</li>
-                        <li>Unsolved</li>
-                        <li>Fictional</li>
+                        <li>
+                            <a href="">Dark Web</a>
+                        </li>
+                        <li>
+                            <a href="">Internet Culture</a>
+                        </li>
+                        <li>
+                            <a href="">Cybersecurity</a>
+                        </li>
+                        <li>
+                            <a href="">Media/Entertainment</a>
+                        </li>
+                        <li>
+                            <a href="">Scary</a>
+                        </li>
+                        <li>
+                            <a href="">Conspiracies</a>
+                        </li>
+                        <li id="last">Fictional<a href="">
+                        </a>
+                        </li>
                     </ul>
                 </div>
             </button>
@@ -72,24 +90,38 @@ function MiniNav() {
 //in column format
 //default sort is by most popular that day
 function ScrollFeed() {
+    let article_author = "me"
     return (
         <main className='flex flex-column bg-zinc-700 max-w-4xl justify-center m-auto mt-5 overflow-y-auto rounded-3xl'>
             <section className='bg-zinc-600 w-MainScroll mt-5 rounded-3xl'>
-                <div className='space-y-40 mx-5'>
+                <div className='space-y-16 mx-5'>
                     <article>
-                        <h1><b>title</b></h1>
-                        <p>test</p>
-                        <small>tags go here</small>
+                        <h1><b>Are they Training an AI Model on You?</b></h1>
+                        <br></br>
+                        <p>Is it true that scientists are secretly training an AI model on people without their consent?
+                            Some individuals claim they are being used as data sources and notice strange things happening in their lives. </p>
+                        <br></br>
+                        <small>tags: conspiracy, cybersecurity</small>
+                        <br></br>
+                        <small><cite>Submitted by <b>{article_author}</b></cite></small>
                     </article>
                     <article>
-                        <h1><b>title</b></h1>
-                        <p>test2</p>
-                        <small>tags go here</small>
+                        <h1><b>Red Rooms: New Evidence Found?</b></h1>
+                        <br></br>
+                        <p>A group of individuals on Reddit claim to have discovered what could be proof of the existence of
+                            the infamous "red rooms", a place hidden in the dark web where murders are broadcasted live.
+                        </p>
+                        <br></br>
+                        <small>tags: scary, internet culture, dark web</small>
+                        <br></br>
+                        <small><cite>Submitted by <b>{article_author}</b></cite></small>
                     </article>
                     <article>
                         <h1><b>title</b></h1>
                         <p>test3</p>
                         <small>tags go here</small>
+                        <br></br>
+                        <small><cite>Submitted by <b>{article_author}</b></cite></small>
                     </article>
                 </div>
             </section>
